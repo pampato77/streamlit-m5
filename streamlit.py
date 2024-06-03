e6e1a846df3673730462081798d8e9e0eddb4483
pampato77 <141702046+pampato77@users.noreply.github.com>
2024-06-03 14:01:13 -0600

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -44,7 +44,7 @@ fig_distribution_perf=px.bar(df_selection,
 performance_score="Puntaje de desempeño"),
                              color_discrete_sequence=["#7EACCB"],
                              template="plotly_white")
-fig_distribution_perf.update_layout(plot_bgcolor="rgba(1,2,3,4)")
+fig_distribution_perf.update_layout(plot_bgcolor="rgba(0,0,0,0)")
 st.plotly_chart(fig_distribution_perf)
 
 #Grafica del promedio de horas trabajadas por el genero del empleado
@@ -58,7 +58,7 @@ fig_hours_gender=px.bar(avg_hours_gender,
                         orientation="v",
                         title="Promedio de horas trabajadas por genero",
                         labels=dict(average_work_hours="Total Worked Hours", gender="Gender"),
-                        color_discrete_sequence=["#F2D44B"],
+                        color_discrete_sequence=["#A1FC9D"],
                         template="plotly_white")
 
 fig_hours_gender.update_layout(plot_bgcolor="rgba(0,0,0,0)")
